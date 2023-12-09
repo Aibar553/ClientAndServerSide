d8aa54c1b32855c0bf309b8a8b56cc7d6eb9cb0e
Aibar553 <74353064+Aibar553@users.noreply.github.com>
2023-12-09 07:37:58 +0600

    Update Program.cs

--- ClientSide/ClientSide/Program.cs
+++ ClientSide/ClientSide/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Sockets;
 using System.Text;
 
@@ -18,23 +18,32 @@ class Client
 
             NetworkStream stream = client.GetStream();
 
-            // Send message to the server
-            Console.Write("Enter a message to send: ");
-            string message = Console.ReadLine();
-            byte[] data = Encoding.Unicode.GetBytes(message);
-            stream.Write(data, 0, data.Length);
-            Console.WriteLine($"Sent to server: {message}");
+            while (true)
+            {
+                // Send message to the server
+                Console.Write("Enter a message to send: (type 'exit' to close):");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Sent to server: {message}");
 
-            // Wait for acknowledgment from the server
-            data = new byte[256];
-            int bytesRead = stream.Read(data, 0, data.Length);
-            string serverAck = Encoding.Unicode.GetString(data, 0, bytesRead);
-            Console.WriteLine($"Acknowledgment from server: {serverAck}");
+                // Check if the user wants to exit
+                if(message.ToLower() == "exit")
+                {
+                    Console.WriteLine("Exiting the client application...");
+                    break;
+                }
+                // Wait for acknowledgment from the server
+                data = new byte[256];
+                int bytesRead = stream.Read(data, 0, data.Length);
+                string serverAck = Encoding.Unicode.GetString(data, 0, bytesRead);
+                Console.WriteLine($"Acknowledgment from server: {serverAck}");
 
-            // Send acknowledgment to the server
-            string acknowledgment = "Acknowledgment from client.";
-            data = Encoding.Unicode.GetBytes(acknowledgment);
-            stream.Write(data, 0, data.Length);
+                // Send acknowledgment to the server
+                string acknowledgment = "Acknowledgment from client.";
+                data = Encoding.Unicode.GetBytes(acknowledgment);
+                stream.Write(data, 0, data.Length);
+            }
 
             // Close the connection with the server
             client.Close();
