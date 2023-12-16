d68ce3b0ca1b2e29c1b5229447661ca937e3ca08
Aibar553 <74353064+Aibar553@users.noreply.github.com>
2023-12-16 10:41:48 +0600

    Update Program.cs

--- ServerSide/ServerSide/Program.cs
+++ ServerSide/ServerSide/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -6,13 +6,20 @@ using System.Threading;
 
 class UdpServer
 {
-    static async Task Main()
-    {
-        var cts = new CancellationTokenSource();
+    private readonly UdpClient udpClient;
+    private readonly CancellationTokenSource cts;
 
+    public UdpServer(int port)
+    {
+        udpClient = new UdpClient(port);
+        cts = new CancellationTokenSource();
+    }
+
+    public async Task StartAsync()
+    {
         Console.WriteLine("UDP Server is running. Press any key to stop.");
 
-        var serverTask = StartServerAsync(cts.Token);
+        var serverTask = StartListeningAsync(cts.Token);
 
         // Wait for a key to be pressed to stop the server
         Console.ReadKey();
@@ -22,36 +29,42 @@ class UdpServer
         await serverTask;
     }
 
-    static async Task StartServerAsync(CancellationToken cancellationToken)
+    private async Task StartListeningAsync(CancellationToken cancellationToken)
     {
-        using (var udpClient = new UdpClient(12345))
+        try
         {
-            try
+            while (!cancellationToken.IsCancellationRequested)
             {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    Console.WriteLine("Waiting for a message...");
+                Console.WriteLine("Waiting for a message...");
 
-                    var receiveResult = await udpClient.ReceiveAsync();
+                var receiveResult = await udpClient.ReceiveAsync();
 
-                    var receivedMessage = Encoding.UTF8.GetString(receiveResult.Buffer);
-                    Console.WriteLine($"Received message: {receivedMessage} from {receiveResult.RemoteEndPoint}");
+                var receivedMessage = Encoding.UTF8.GetString(receiveResult.Buffer);
+                Console.WriteLine($"Received message: {receivedMessage} from {receiveResult.RemoteEndPoint}");
 
-                    // Send confirmation back to the client
-                    var confirmationMessage = $"Server received: {receivedMessage}";
-                    var confirmationBytes = Encoding.UTF8.GetBytes(confirmationMessage);
-                    await udpClient.SendAsync(confirmationBytes, confirmationBytes.Length, receiveResult.RemoteEndPoint);
+                // Send confirmation back to the client
+                var confirmationMessage = $"Server received: {receivedMessage}";
+                var confirmationBytes = Encoding.UTF8.GetBytes(confirmationMessage);
+                await udpClient.SendAsync(confirmationBytes, confirmationBytes.Length, receiveResult.RemoteEndPoint);
 
-                    if (receivedMessage.Equals("Exit", StringComparison.OrdinalIgnoreCase))
-                        break;
-                }
+                if (receivedMessage.Equals("Exit", StringComparison.OrdinalIgnoreCase))
+                    break;
             }
-            catch (ObjectDisposedException)
-            {
-                // Ignore the ObjectDisposedException when the UdpClient is disposed
-            }
+        }
+        catch (ObjectDisposedException)
+        {
+            // Ignore the ObjectDisposedException when the UdpClient is disposed
         }
 
         Console.WriteLine("Server stopped.");
+    }
+}
+
+class Program
+{
+    static async Task Main()
+    {
+        var chatServer = new UdpServer(12345);
+        await chatServer.StartAsync();
     }
 }
