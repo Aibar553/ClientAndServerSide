9b2c80387545e78b88472b0cadd3a18450aac6ec
Aibar553 <74353064+Aibar553@users.noreply.github.com>
2023-12-16 10:28:21 +0600

    Add files via upload

---
+++ ClientSide/ClientSide/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+
+class UdpClientApp
+{
+        static async Task Main()
+        {
+            using (var udpClient = new UdpClient())
+            {
+                var serverEndpoint = new IPEndPoint(IPAddress.Loopback, 12345);
+
+                Console.WriteLine("UDP Client is running. Type 'Exit' to stop.");
+
+                while (true)
+                {
+                    Console.Write("Enter a message: ");
+                    var message = Console.ReadLine();
+
+                    var messageBytes = Encoding.UTF8.GetBytes(message);
+
+                    await udpClient.SendAsync(messageBytes, messageBytes.Length, serverEndpoint);
+
+                    if (message.Equals("Exit", StringComparison.OrdinalIgnoreCase))
+                        break;
+
+                    // Receive confirmation from the server
+                    var receiveResult = await udpClient.ReceiveAsync();
+                    var confirmationMessage = Encoding.UTF8.GetString(receiveResult.Buffer);
+                    Console.WriteLine($"Server confirmation: {confirmationMessage}");
+                }
+            }
+
+            Console.WriteLine("Client stopped.");
+        }
+    }
---
+++ ServerSide/ServerSide/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class UdpServer
+{
+    static async Task Main()
+    {
+        var cts = new CancellationTokenSource();
+
+        Console.WriteLine("UDP Server is running. Press any key to stop.");
+
+        var serverTask = StartServerAsync(cts.Token);
+
+        // Wait for a key to be pressed to stop the server
+        Console.ReadKey();
+        cts.Cancel();
+
+        // Wait for the server task to complete
+        await serverTask;
+    }
+
+    static async Task StartServerAsync(CancellationToken cancellationToken)
+    {
+        using (var udpClient = new UdpClient(12345))
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("Waiting for a message...");
+
+                    var receiveResult = await udpClient.ReceiveAsync();
+
+                    var receivedMessage = Encoding.UTF8.GetString(receiveResult.Buffer);
+                    Console.WriteLine($"Received message: {receivedMessage} from {receiveResult.RemoteEndPoint}");
+
+                    // Send confirmation back to the client
+                    var confirmationMessage = $"Server received: {receivedMessage}";
+                    var confirmationBytes = Encoding.UTF8.GetBytes(confirmationMessage);
+                    await udpClient.SendAsync(confirmationBytes, confirmationBytes.Length, receiveResult.RemoteEndPoint);
+
+                    if (receivedMessage.Equals("Exit", StringComparison.OrdinalIgnoreCase))
+                        break;
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                // Ignore the ObjectDisposedException when the UdpClient is disposed
+            }
+        }
+
+        Console.WriteLine("Server stopped.");
+    }
+}
