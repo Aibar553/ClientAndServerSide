e3d1d87ac650b3696275c16ee4c7a74f5ae99d66
Aibar553 <74353064+Aibar553@users.noreply.github.com>
2023-12-16 10:42:25 +0600

    Update Program.cs

--- ClientSide/ClientSide/Program.cs
+++ ClientSide/ClientSide/Program.cs
@@ -1,37 +1,49 @@
-﻿using System.Net.Sockets;
+using System.Net.Sockets;
 using System.Net;
 using System.Text;
 
 
 class UdpClientApp
 {
-        static async Task Main()
-        {
-            using (var udpClient = new UdpClient())
-            {
-                var serverEndpoint = new IPEndPoint(IPAddress.Loopback, 12345);
+    private readonly UdpClient udpClient;
+    private readonly IPEndPoint serverEndpoint;
 
-                Console.WriteLine("UDP Client is running. Type 'Exit' to stop.");
-
-                while (true)
-                {
-                    Console.Write("Enter a message: ");
-                    var message = Console.ReadLine();
-
-                    var messageBytes = Encoding.UTF8.GetBytes(message);
-
-                    await udpClient.SendAsync(messageBytes, messageBytes.Length, serverEndpoint);
-
-                    if (message.Equals("Exit", StringComparison.OrdinalIgnoreCase))
-                        break;
-
-                    // Receive confirmation from the server
-                    var receiveResult = await udpClient.ReceiveAsync();
-                    var confirmationMessage = Encoding.UTF8.GetString(receiveResult.Buffer);
-                    Console.WriteLine($"Server confirmation: {confirmationMessage}");
-                }
-            }
-
-            Console.WriteLine("Client stopped.");
-        }
+    public UdpClientApp(string serverIpAddress, int serverPort)
+    {
+        udpClient = new UdpClient();
+        serverEndpoint = new IPEndPoint(IPAddress.Parse(serverIpAddress), serverPort);
     }
+
+    public async Task StartAsync()
+    {
+        Console.WriteLine("UDP Client is running. Type 'Exit' to stop.");
+
+        while (true)
+        {
+            Console.Write("Enter a message: ");
+            var message = Console.ReadLine();
+
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+
+            await udpClient.SendAsync(messageBytes, messageBytes.Length, serverEndpoint);
+
+            if (message.Equals("Exit", StringComparison.OrdinalIgnoreCase))
+                break;
+
+            // Receive confirmation from the server
+            var receiveResult = await udpClient.ReceiveAsync();
+            var confirmationMessage = Encoding.UTF8.GetString(receiveResult.Buffer);
+            Console.WriteLine($"Server confirmation: {confirmationMessage}");
+        }
+
+        Console.WriteLine("Client stopped.");
+    }
+}
+class Program
+{
+    static async Task Main()
+    {
+        var chatClient = new UdpClientApp("127.0.0.1", 12345);
+        await chatClient.StartAsync();
+    }
+}
